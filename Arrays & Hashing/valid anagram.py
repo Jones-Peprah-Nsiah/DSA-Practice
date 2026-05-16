14cfe3b374c78cc66b8cc6af8e7cdad9ed455f10
Jones <jones@Joness-MacBook-Air.local>
2026-05-16 16:52:46 -0500

    Add valid anagram to Arrays & Hashing

---
+++ Arrays & Hashing/valid anagram.py
@@ -0,0 +1,21 @@
+
+# 242. Valid Anagram
+# https://leetcode.com/problems/valid-anagram/
+#
+# Time: O(n)
+# Space: O(n)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        count_s,count_t={},{}
+
+        for i in range(len(s)):
+            count_s[s[i]]=count_s.get(s[i],0)+1
+            count_t[t[i]]=count_t.get(t[i],0)+1
+
+        for c in count_s:
+            if count_s[c]!=count_t.get(c,0):
+                return False
+        return True
